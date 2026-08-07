a50c83ca733a5961e2878947b52b072e72aa3c15
jvnsn <joviansenwidyanto@google.com>
2026-08-07 22:33:43 +0700

    Make List Files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,18 +2,27 @@ import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
-        if not os.path.isdir(directory):
-            return f'Error: "{directory}" is not a directory'
-
+        list_files = []
         working_dir_abs = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
+        if not os.path.isdir(target_dir):
+            return f'Error: "{target_dir}" is not a directory'
+        
         if not valid_target_dir:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         else:
-            return f'Success: "{directory}" is within the working directory'
+            list = os.listdir(target_dir)
+            for item in list:     
+                fullpath = os.path.join(target_dir, item)
+                filesize = os.path.getsize(fullpath)
+                isdir = os.path.isdir(fullpath)
+                list_files.append(f"- {item}: file_size={filesize} bytes, is_dir={isdir}")
+            return "\n".join(list_files)
+
         
+
     except Exception as e:
         return f'Error: {e}'
     
